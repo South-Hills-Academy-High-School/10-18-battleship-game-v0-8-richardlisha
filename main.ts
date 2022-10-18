fd9e4a52225428259b95406e5278c0fa25e81aed
richardlisha <113388339+richardlisha@users.noreply.github.com>
2022-10-18 14:16:35 -0700

    fixed rotate bug (bottom screen)

--- main.ts
+++ main.ts
@@ -5,6 +5,7 @@ namespace SpriteKind {
     export const Boat2 = SpriteKind.create()
 }
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    rotateFlag = "nothing"
     grid.move(cursor, 0, -1)
     grid.place(shadowCursor, tiles.getTileLocation(grid.spriteCol(cursor), grid.spriteRow(cursor) + 1))
 })
@@ -30,20 +31,30 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     }
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    rotateFlag = "nothing"
     grid.move(cursor, -1, 0)
     grid.place(shadowCursor, tiles.getTileLocation(grid.spriteRow(cursor) + 1, grid.spriteCol(cursor)))
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    rotateFlag = "nothing"
     grid.move(cursor, 1, 0)
     grid.place(shadowCursor, tiles.getTileLocation(grid.spriteRow(cursor) + -1, grid.spriteCol(cursor)))
 })
 function moveBoat (boatArray: any[]) {
     makeBoatVisible(boatArray)
     if (grid.spriteRow(cursor) >= 8 - boatArray.length && boatRotateArray[currentBoat] == "up") {
-        grid.move(cursor, 0, -1)
+        if (rotateFlag != "nothing") {
+            boatRotateArray[currentBoat] = rotateFlag
+        } else {
+            grid.move(cursor, 0, -1)
+        }
     }
     if (grid.spriteCol(cursor) >= 11 - boatArray.length && boatRotateArray[currentBoat] == "sideways") {
-        grid.move(cursor, -1, 0)
+        if (rotateFlag != "nothing") {
+            boatRotateArray[currentBoat] = rotateFlag
+        } else {
+            grid.move(cursor, -1, 0)
+        }
     }
     cursor.setFlag(SpriteFlag.Invisible, true)
     iterator = 0
@@ -57,6 +68,7 @@ function moveBoat (boatArray: any[]) {
     }
 }
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    rotateFlag = "nothing"
     grid.move(cursor, 0, 1)
     grid.place(shadowCursor, tiles.getTileLocation(grid.spriteCol(cursor), grid.spriteRow(cursor) + -1))
 })
